5a5d77199deea622ca92e1ea7d5ffed053598291
vmrozhkovsky <128725885+vmrozhkovsky@users.noreply.github.com>
2024-01-09 23:19:57 +0300

    Хедер, сайдбар и футер вынесены в отдельные файлы. Код на JS для главной и тестовой страницы. Код для поиска.

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,10 @@
+﻿// let userAge = prompt("Введите свой реальный возраст");
+// if (userAge < 18) {
+//     window.location.href = "http://www.google.com";}
+// else {
+//     alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());}
+document.getElementsByName("").addEventListener("input", myFunction);
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "The value of the input field was changed.";
+}
---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,7 @@
+﻿// let userName = prompt("Введите своё имя:");
+// let nameLength = userName.length;
+// alert(`Ваше имя ${userName}.\nВ нем ${nameLength} букв.`);
+
+let htmlElements = document.getElementsByTagName('*');
+console.log(htmlElements);
+alert(`Количество HTML элементов на странице:  ${htmlElements.length}`);
