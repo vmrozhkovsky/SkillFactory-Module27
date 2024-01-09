2bcb12e8cc9c8283990cfc7597c21e052d999ae5
vmrozhkovsky <128725885+vmrozhkovsky@users.noreply.github.com>
2024-01-09 23:42:20 +0300

    Код для поиска перенесен в файл js.

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,10 +1,17 @@
-﻿// let userAge = prompt("Введите свой реальный возраст");
-// if (userAge < 18) {
-//     window.location.href = "http://www.google.com";}
-// else {
-//     alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());}
-document.getElementsByName("").addEventListener("input", myFunction);
+﻿document.getElementById("search").oninput = function() {myFunction()};
 
 function myFunction() {
-    document.getElementById("demo").innerHTML = "The value of the input field was changed.";
+    console.log('Start');
+    let elements = document.getElementsByTagName('input');
+    let element = elements[0];
+    let elementText = element.value;
+    let videoElements = document.body.getElementsByClassName('video-container');
+    for (let i = 0; i < videoElements.length; i++)
+    {
+        let childElements = videoElements[i];
+        let videoDescription = childElements.getElementsByTagName('h3')[0];
+        if (!(videoDescription.innerText.toLowerCase().includes(elementText.toLowerCase()))){
+            videoElements[i].style.display = 'none';
+        }
+    }
 }
