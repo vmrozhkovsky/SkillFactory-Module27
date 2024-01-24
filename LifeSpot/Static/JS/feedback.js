2f99324aab65e0c4bc26750572f9077b2c895668
vmrozhkovsky <128725885+vmrozhkovsky@users.noreply.github.com>
2024-01-24 23:14:59 +0300

    Добавлена возможность ставить лайки для отзывов.

--- LifeSpot/Static/JS/feedback.js
+++ LifeSpot/Static/JS/feedback.js
@@ -1,33 +1,39 @@
-﻿function getFeedback () {
-    let feedback = {};
-    
-    feedback['UserName'] = prompt("Введите Ваше имя:");
-    if(feedback["UserName"] == null){
+﻿function Feedback () {
+    this.UserName = prompt("Введите Ваше имя:");
+    if (this.UserName == null) {
         return
     }
 
-    feedback['UserFeedbackText'] = prompt("Напишите свой отзыв:");
-    if(feedback['UserFeedbackText'] == null){
+    this.UserFeedbackText = prompt("Напишите свой отзыв:");
+    if (this.UserFeedbackText == null) {
         return
     }
-    
-    feedback['FeedbackDate'] = new Date().toLocaleString();
 
+    this.FeedbackDate = new Date().toLocaleString();
+}
+function GetFeedback (){
+    let feedback = new Feedback();
     if(confirm('Разрешить пользователям оценивать ваш отзыв?')){
-        let feedbackWithRating = Object.create(feedback);
-        feedbackWithRating['Rating'] = 0;
-        writeFeedback(feedbackWithRating);
-    } else{
-        writeFeedback(feedback);
-    }
+    feedback['Rating'] = 0;
+}
+    writeFeedback(feedback);
+}
+function addLike(id){
+    let element = document.getElementById(id);
+    let array = element.innerText.split(' ')
+    let resultNum = parseInt(array[array.length - 1], 10);
+    resultNum += 1
+    array[array.length - 1] =  `${resultNum}`
+    element.innerText = array.join(' ')
 }
 const writeFeedback = feedback => {
     let ratingCounter = '';
     if (feedback.hasOwnProperty('Rating')){
-        ratingCounter += '           <b style="color: chocolate">Рейтинг:</b>   ' + feedback.Rating;
+        let feedbackId = Math.random();
+        ratingCounter += '<button class="rating" id="' + feedbackId + '" onclick="addLike(this.id)">' + `❤️ ${feedback.Rating}</button>`;
     }
     document.getElementsByClassName('feedback')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${feedback['UserName']}</b>  ${feedback['FeedbackDate']}${ratingCounter}</i></p>` +
+        `<p> <i> <b>${feedback['UserName']}</b>  ${feedback['FeedbackDate']} ${ratingCounter}</i></p>` +
         `<p>${feedback['UserFeedbackText']}</p>`  +
         '</div>';
 }
